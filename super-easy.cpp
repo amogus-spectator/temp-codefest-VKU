d243f9f1bb48fbe95da6d4d62fc9a344c8f91a80
amogus-spectator <177993284+amogus-spectator@users.noreply.github.com>
2025-03-09 17:24:25 +0700

    Update super-easy.cpp

--- super-easy.cpp
+++ super-easy.cpp
@@ -2,8 +2,8 @@
 
 using namespace std;
 
-float calculateArea(int* a) {
-    float f = float(((*a) * (*a)) / 4.0);
+float calculateArea(int& a) {
+    float f = float(((a * a)) / 4.0);
     return f;
 }
 
@@ -14,7 +14,7 @@ int main() {
         if ((n <= 0) || (n > 1000)) {
             throw 0;
         } else {
-            cout << calculateArea(&n) << endl;
+            cout << calculateArea(n) << endl;
         }
     } catch (int error) {
         cout << "Invalid input" << endl;
